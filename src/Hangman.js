5b1f52fa4cd033c5dd77e2de51b279851fdcd89f
Engi <engilozada@gmail.com>
2022-11-26 21:54:59 +0100

    first commit

---
+++ src/Hangman.js
@@ -0,0 +1,83 @@
+import React, { Component } from "react";
+import "./Hangman.css";
+import img0 from "./0.jpg";
+import img1 from "./1.jpg";
+import img2 from "./2.jpg";
+import img3 from "./3.jpg";
+import img4 from "./4.jpg";
+import img5 from "./5.jpg";
+import img6 from "./6.jpg";
+import { randomWord } from "./words.js";
+
+class Hangman extends Component {
+  static defaultProps = {
+    maxWrong: 6,
+    images: [img0, img1, img2, img3, img4, img5, img6],
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = { nWrong: 0, guessed: new Set(), answer: randomWord() };
+    this.handleGuess = this.handleGuess.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+  }
+
+  guessedWord() {
+    return this.state.answer
+      .split("")
+      .map((ltr) => (this.state.guessed.has(ltr) ? ltr : "_"));
+  }
+
+  handleGuess(evt) {
+    let ltr = evt.target.value;
+    this.setState((st) => ({
+      guessed: st.guessed.add(ltr),
+      nWrong: st.nWrong + (st.answer.includes(ltr) ? 0 : 1),
+    }));
+  }
+
+  generateButtons() {
+    return "abcdefghijklmnopqrstuvwxyz".split("").map((ltr) => (
+      <button
+        key={ltr}
+        value={ltr}
+        onClick={this.handleGuess}
+        disabled={this.state.guessed.has(ltr)}
+      >
+        {ltr}
+      </button>
+    ));
+  }
+
+  reset() {
+    this.setState({ nWrong: 0, guessed: new Set(), answer: randomWord() });
+  }
+  handleReset() {
+    this.reset();
+  }
+
+  render() {
+    const altText = `${this.state.nWrong}/${this.props.maxWrong}`;
+    const gameOver = this.state.nWrong >= this.props.maxWrong;
+    const isWinner = this.guessedWord().join("") === this.state.answer;
+    let gameState = this.generateButtons();
+    if (isWinner) gameState = "You have won";
+    if (gameOver) gameState = "You have lost";
+    return (
+      <div className="Hangman">
+        <h1>Hangman</h1>
+        <img src={this.props.images[this.state.nWrong]} alt={altText} />
+        <p>You have {this.state.nWrong} errors</p>
+        <p className="Hangman-word">
+          {!gameOver ? this.guessedWord() : this.state.answer}
+        </p>
+        <p className="Hangman-btns">{gameState}</p>
+        <button id="reset" onClick={this.handleReset}>
+          Restart?
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Hangman;
